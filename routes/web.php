4ffd0a045edb1fa6dbee29633540854a7c1daa5b
antlerjely96 <antlerjely96@gmail.com>
2026-03-16 10:39:57 +0700

    Demo Route Group

--- routes/web.php
+++ routes/web.php
@@ -6,41 +6,53 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route hiển thị danh sách
-Route::get('/brands', [\App\Http\Controllers\BrandController::class, 'index'])
-    ->name('brands.index');
-//Route hiển thị form thêm
-Route::get('/brands/create', [\App\Http\Controllers\BrandController::class, 'create'])
-    ->name('brands.create');
-//Route thêm dữ liệu
-Route::post('/brands/create', [\App\Http\Controllers\BrandController::class, 'store'])
-    ->name('brands.store');
-//Route hiển thị form sửa
-Route::get('/brands/{brand}/edit', [\App\Http\Controllers\BrandController::class, 'edit'])
-    ->name('brands.edit');
-//Route update dữ liệu
-Route::put('/brands/{brand}/edit', [\App\Http\Controllers\BrandController::class, 'update'])
-    ->name('brands.update');
-//Route delete dữ liệu
-Route::delete('/brands/{brand}', [\App\Http\Controllers\BrandController::class, 'destroy'])
-    ->name('brands.destroy');
+Route::prefix('/admins')
+    ->group(function(){
+        Route::controller(\App\Http\Controllers\BrandController::class)
+            ->name('brands.')
+            ->prefix('/brands')
+            ->group(function(){
+                //Route hiển thị danh sách
+                Route::get('/', 'index')
+                    ->name('index');
+                //Route hiển thị form thêm
+                Route::get('/create', 'create')
+                    ->name('create');
+                //Route thêm dữ liệu
+                Route::post('/create', 'store')
+                    ->name('store');
+                //Route hiển thị form sửa
+                Route::get('/{brand}/edit', 'edit')
+                    ->name('edit');
+                //Route update dữ liệu
+                Route::put('/{brand}/edit', 'update')
+                    ->name('update');
+                //Route delete dữ liệu
+                Route::delete('/{brand}', 'destroy')
+                    ->name('destroy');
+            });
 
-
-//Route hiển thị shoes
-Route::get('/shoes', [\App\Http\Controllers\ShoeController::class, 'index'])
-    ->name('shoes.index');
-//Route hiển thị form thêm
-Route::get('/shoes/create', [\App\Http\Controllers\ShoeController::class, 'create'])
-    ->name('shoes.create');
-//Route thêm dữ liệu
-Route::post('/shoes/create', [\App\Http\Controllers\ShoeController::class, 'store'])
-    ->name('shoes.store');
-//Route hiển thị form sửa
-Route::get('shoes/{shoe}/edit', [\App\Http\Controllers\ShoeController::class, 'edit'])
-    ->name('shoes.edit');
-//Route update dữ liệu
-Route::put('shoes/{shoe}/edit', [\App\Http\Controllers\ShoeController::class, 'update'])
-    ->name('shoes.update');
-//Route delete dữ liệu
-Route::delete('shoes/{shoe}', [\App\Http\Controllers\ShoeController::class, 'destroy'])
-    ->name('shoes.destroy');
+        Route::controller(\App\Http\Controllers\ShoeController::class)
+            ->name('shoes.')
+            ->prefix('/shoes')
+            ->group(function(){
+                //Route hiển thị shoes
+                Route::get('/', 'index')
+                    ->name('index');
+                //Route hiển thị form thêm
+                Route::get('/create', 'create')
+                    ->name('create');
+                //Route thêm dữ liệu
+                Route::post('/create', 'store')
+                    ->name('store');
+                //Route hiển thị form sửa
+                Route::get('/{shoe}/edit', 'edit')
+                    ->name('edit');
+                //Route update dữ liệu
+                Route::put('/{shoe}/edit', 'update')
+                    ->name('update');
+                //Route delete dữ liệu
+                Route::delete('/{shoe}',  'destroy')
+                    ->name('destroy');
+            });
+    });
